b395f8dbba5deaaa78c2cbbf7ec8e95d0cb19a35
playoffp <popo9013163@gmail.com>
2022-04-13 22:19:39 +0800

    ksecondarycolor

--- firstapp/lib/constants.dart
+++ firstapp/lib/constants.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 // Colors that we use in our app
 const kPrimaryColor = Color.fromARGB(255, 98, 152, 168);
+const kSecondaryColor = Color.fromARGB(255, 193, 216, 223);
 const kTextColor = Color(0xFF3C4046);
 const kBackgroundColor = Color(0xFFF9F8FD);
 
