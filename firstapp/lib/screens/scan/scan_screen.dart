001458f7f4feab004236bda91b7420cde0b383ef
playoffp <popo9013163@gmail.com>
2022-04-28 02:08:12 +0800

    home screen

--- firstapp/lib/screens/scan/scan_screen.dart
+++ firstapp/lib/screens/scan/scan_screen.dart
@@ -11,58 +11,58 @@ class ScanScreen extends StatefulWidget {
 }
 
 class _IdentityPageState extends State<ScanScreen> {
-
   Future<void> _handleRefresh() async {
-    return await Future.delayed(Duration(seconds: 2 ));
+    return await Future.delayed(Duration(seconds: 1));
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-      backgroundColor: Color(0xFFF9F8FD),
-      body : LiquidPullToRefresh(
-        onRefresh: _handleRefresh,
-        color: Color.fromARGB(255, 98, 152, 168),
-        height: 180,
-        backgroundColor: Color.fromARGB(255, 193, 216, 223),
-        animSpeedFactor: 2.0,
-        showChildOpacityTransition: false,
-        child: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Container(
-                  height: 300,
-                  color: Color.fromARGB(255, 98, 152, 168),
-                 ),
+    return Scaffold(
+        backgroundColor: Color(0xFFF9F8FD),
+        body: LiquidPullToRefresh(
+          onRefresh: _handleRefresh,
+          color: Color.fromARGB(255, 98, 152, 168),
+          height: 180,
+          backgroundColor: Color.fromARGB(255, 193, 216, 223),
+          animSpeedFactor: 2.0,
+          showChildOpacityTransition: false,
+          child: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Container(
+                    height: 300,
+                    color: Color.fromARGB(255, 98, 152, 168),
+                  ),
+                ),
               ),
-            ),   
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Container(
-                  height: 300,
-                  color: Color.fromARGB(255, 98, 152, 168),
-                 ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Container(
+                    height: 300,
+                    color: Color.fromARGB(255, 98, 152, 168),
+                  ),
+                ),
               ),
-            ),     
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Container(
-                  height: 300,
-                  color: Color.fromARGB(255, 98, 152, 168),
-                 ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Container(
+                    height: 300,
+                    color: Color.fromARGB(255, 98, 152, 168),
+                  ),
+                ),
               ),
-            ),         
-          ],
-        ),
-      )
-      //appBar: buildAppBar(), 
-    );
+            ],
+          ),
+        )
+        //appBar: buildAppBar(),
+        );
   }
 
   AppBar buildAppBar() {
