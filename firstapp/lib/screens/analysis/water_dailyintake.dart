8a04fc0358ba9841b763a47d9f5410751c95ba6d
playoffp <popo9013163@gmail.com>
2022-10-05 21:13:01 +0800

    analysis detail ui/crawler

--- firstapp/lib/screens/analysis/water_dailyintake.dart
+++ firstapp/lib/screens/analysis/water_dailyintake.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:percent_indicator/percent_indicator.dart';
@@ -97,108 +97,113 @@ class _WaterIntake extends State<WaterIntake> {
   @override
   Widget build(BuildContext context) {
     //initTimer();
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 207, 219, 235),
-        borderRadius: BorderRadius.circular(20),
-        //boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 3)],
-      ),
-      padding: const EdgeInsets.all(16),
-      //borderRadius: BorderRadius.circular(8.0),
-      //color: Color(0xFFE3F2FD),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        //mainAxisAlignment: MainAxisAlignment.start,
-        //mainAxisSize: MainAxisSize.max,
-        children: [
-          const Text(
-            'Water Intake',
-            style: TextStyle(
-                color: Color.fromARGB(255, 87, 96, 120),
-                fontSize: 24,
-                fontWeight: FontWeight.bold),
-          ),
-          Container(height: 25),
-          CircularPercentIndicator(
-            radius: 100.0,
-            lineWidth: 40.0,
-            animation: true,
-            backgroundColor: const Color.fromARGB(255, 230, 239, 247),
-            percent: (_total / 2000),
-            center: Text(
-              ((((_total / 2000) * 100).toStringAsFixed(2))).toString() + "%",
-              style: const TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.w600,
-                  color: Color.fromARGB(255, 87, 104, 120)),
-            ),
-            circularStrokeCap: CircularStrokeCap.round,
-            progressColor: const Color.fromARGB(255, 114, 166, 209),
-          ),
-          Container(height: 25),
-          //Text("shared_preferences儲存", textAlign: TextAlign.center),
-          TextField(
-              controller: _textFieldController,
-              keyboardType: TextInputType.number,
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly
-              ],
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  contentPadding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  labelText: "飲水量 (ml)",
-                  labelStyle: const TextStyle(
-                      color: Color.fromARGB(255, 140, 152, 189)),
-                  prefixIcon: const Icon(Icons.local_drink,
-                      color: Color.fromARGB(255, 140, 152, 189)),
-                  border: myinputborder(),
-                  enabledBorder: myinputborder(),
-                  focusedBorder: myfocusborder(),
-                  suffixIcon: IconButton(
-                    icon: const Icon(Icons.check_circle),
-                    color: const Color.fromARGB(255, 140, 152, 189),
-                    onPressed: () {
-                      _ml = double.parse(_textFieldController.text);
-                      Testnow = DateTime.now();
-                      reset =
-                          DateTime(Testnow.year, Testnow.month, Testnow.day);
-                      temp = reset.toString();
-                      if (_storageDate != temp.toString()) {
-                        _total = 0;
-                        if (_total + _ml > 2000) {
-                          _total = 2000;
-                          mls = 2000;
-                          _textFieldController.text = "";
-                        } else if (_total + _ml <= 2000) {
-                          _total = _total + _ml;
-                          mls = _total;
-                          _textFieldController.text = "";
-                        }
-                        saveString();
-                        getString();
-                      } else {
-                        if (_total + _ml > 2000) {
-                          _total = 2000;
-                          mls = 2000;
-                          _textFieldController.text = "";
-                        } else if (_total + _ml <= 2000) {
-                          _total = _total + _ml;
-                          mls = _total;
-                          _textFieldController.text = "";
-                        }
-                        debugPrint('一樣 $temp');
-                        saveString();
-                        getString();
-                      }
-                      DateTime now = DateTime.now();
-                      datestamp =
-                          (DateTime(now.year, now.month, now.day)).toString();
-                      saveString();
-                      getString();
-                    },
-                  ))),
-          /*MaterialButton(
+    return Neumorphic(
+        style: NeumorphicStyle(
+            shape: NeumorphicShape.concave,
+            intensity: 0.9,
+            boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(25)),
+            depth: 10,
+            lightSource: LightSource.topRight,
+            color: const Color.fromARGB(255, 207, 219, 235)),
+        child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            //mainAxisAlignment: MainAxisAlignment.start,
+            //mainAxisSize: MainAxisSize.max,
+            children: [
+              const Text(
+                '飲水量紀錄',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 87, 96, 120),
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(height: 15),
+              CircularPercentIndicator(
+                radius: 100.0,
+                lineWidth: 40.0,
+                animation: true,
+                backgroundColor: const Color.fromARGB(255, 230, 239, 247),
+                percent: (_total / 2000),
+                center: Text(
+                  ((((_total / 2000) * 100).toStringAsFixed(2))).toString() +
+                      "%",
+                  style: const TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                      color: Color.fromARGB(255, 87, 104, 120)),
+                ),
+                circularStrokeCap: CircularStrokeCap.round,
+                progressColor: const Color.fromARGB(255, 43, 161, 92),
+              ),
+
+              //Text("shared_preferences儲存", textAlign: TextAlign.center),
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: TextField(
+                    controller: _textFieldController,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        labelText: "飲水量 (ml)",
+                        labelStyle: const TextStyle(
+                            color: Color.fromARGB(255, 140, 152, 189)),
+                        prefixIcon: const Icon(Icons.local_drink,
+                            color: Color.fromARGB(255, 140, 152, 189)),
+                        border: myinputborder(),
+                        enabledBorder: myinputborder(),
+                        focusedBorder: myfocusborder(),
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.check_circle),
+                          color: const Color.fromARGB(255, 140, 152, 189),
+                          onPressed: () {
+                            _ml = double.parse(_textFieldController.text);
+                            Testnow = DateTime.now();
+                            reset = DateTime(
+                                Testnow.year, Testnow.month, Testnow.day);
+                            temp = reset.toString();
+                            if (_storageDate != temp.toString()) {
+                              _total = 0;
+                              if (_total + _ml > 2000) {
+                                _total = 2000;
+                                mls = 2000;
+                                _textFieldController.text = "";
+                              } else if (_total + _ml <= 2000) {
+                                _total = _total + _ml;
+                                mls = _total;
+                                _textFieldController.text = "";
+                              }
+                              saveString();
+                              getString();
+                            } else {
+                              if (_total + _ml > 2000) {
+                                _total = 2000;
+                                mls = 2000;
+                                _textFieldController.text = "";
+                              } else if (_total + _ml <= 2000) {
+                                _total = _total + _ml;
+                                mls = _total;
+                                _textFieldController.text = "";
+                              }
+                              debugPrint('一樣 $temp');
+                              saveString();
+                              getString();
+                            }
+                            DateTime now = DateTime.now();
+                            datestamp = (DateTime(now.year, now.month, now.day))
+                                .toString();
+                            saveString();
+                            getString();
+                          },
+                        ))),
+              ),
+              /*MaterialButton(
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(22.0)),
               minWidth: 10.0,
@@ -211,17 +216,17 @@ class _WaterIntake extends State<WaterIntake> {
               child: new Text("歸零"),
               color: Color.fromARGB(255, 255, 238, 181),
             ),*/
-          /*
+              /*
               Text('shared_preferences儲存的值為  $_storageString'),
               Text(' $mls'),*/
-          /*Text('紀錄喝水時間 $datestamp'),
+              /*Text('紀錄喝水時間 $datestamp'),
             Text('上次儲存時間 $_storageDate'),
             Text('開啟app時間 ' +
                 (DateTime(now.year, now.month, now.day)).toString()),
             Text('$now'),
             Text('$temp'),
             Text('$_total'),*/
-          /*RawMaterialButton(
+              /*RawMaterialButton(
               onPressed: () {
                 if (_storageDate !=
                     (new DateTime(now.year, now.month, now.day)).toString()) {
@@ -239,7 +244,7 @@ class _WaterIntake extends State<WaterIntake> {
               //padding: EdgeInsets.all(1.0),
               shape: CircleBorder(),
             )*/
-          /*ElevatedButton(
+              /*ElevatedButton(
                 onPressed: () {
                   showAlertDialog(context);
                   _total = 0;
@@ -252,36 +257,36 @@ class _WaterIntake extends State<WaterIntake> {
                     primary: Color.fromARGB(255, 109, 158, 214),
                     fixedSize: const Size(10, 10)),
                 child: const Icon(Icons.delete)),*/
-          ElevatedButton(
-              onPressed: () {
-                Testnow = DateTime.now();
-                reset = DateTime(Testnow.year, Testnow.month, Testnow.day);
-                temp = reset.toString();
-                if (_storageDate != temp.toString()) {
-                  _total = 0;
-                  //debugPrint('一樣 $_storageDate 和 $temp');
-                  //debugPrint('$_storageDate');
-                  //debugPrint('不一樣 $Testnow');
-                  saveString();
-                  getString();
-                } else {
-                  _total = double.parse(_storageString);
-                  //debugPrint('不一樣 $_storageDate 和 $temp');
-                  //debugPrint('$_storageDate');
-                  //debugPrint('一樣 $temp');
-                  saveString();
-                  getString();
-                }
-                //debugPrint('$_storageDate');
-              },
-              style: ElevatedButton.styleFrom(
-                  shape: const CircleBorder(),
-                  primary: const Color.fromARGB(255, 109, 158, 214),
-                  fixedSize: const Size(10, 10)),
-              child: const Icon(Icons.restart_alt)),
-        ],
-      ),
-    );
+              ElevatedButton(
+                  onPressed: () {
+                    Testnow = DateTime.now();
+                    reset = DateTime(Testnow.year, Testnow.month, Testnow.day);
+                    temp = reset.toString();
+                    if (_storageDate != temp.toString()) {
+                      _total = 0;
+                      //debugPrint('一樣 $_storageDate 和 $temp');
+                      //debugPrint('$_storageDate');
+                      //debugPrint('不一樣 $Testnow');
+                      saveString();
+                      getString();
+                    } else {
+                      _total = double.parse(_storageString);
+                      //debugPrint('不一樣 $_storageDate 和 $temp');
+                      //debugPrint('$_storageDate');
+                      //debugPrint('一樣 $temp');
+                      saveString();
+                      getString();
+                    }
+                    //debugPrint('$_storageDate');
+                  },
+                  style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      primary: Colors.blueGrey,
+                      fixedSize: const Size(10, 10)),
+                  child: const Icon(Icons.restart_alt)),
+            ],
+          ),
+        ));
   }
 
   showAlertDialog(BuildContext context) {
